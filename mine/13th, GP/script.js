85c9600ea5e752045d8975c90fbd79e599498422
Totally Glitched <divdharshj@gmail.com>
2021-03-02 09:50:54 +0530

    gway

---
+++ mine/13th, GP/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    // let animation = anime({
+    //     targets: 'div',
+    //     translateX: 100,
+    //     borderRadius: 50,
+    //     duration: 2000,
+    //     easing: 'linear',
+    //     direction: 'alternate',
+    //     loop: true
+    // });      
+}
